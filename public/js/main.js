de906912d76e79c6a896d30782f5aea217640df9
enzo grillo <enzogrillo96@gmail.com>
2022-05-06 14:49:04 +0200

    first

---
+++ public/js/main.js
@@ -0,0 +1,65 @@
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let temp;
+
+// temp = a;
+// a = b;
+// b = c;
+// c = temp;
+// console.log(a,b,c);
+
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+// let temp;
+
+// temp = a;
+// a = d;
+// d = temp;
+// temp = b;
+// b = c;
+// c = temp;
+
+// console.log(a, b, c, d)
+
+//  temp = a;
+//  a = c;
+//  c = b;
+//  b = a;
+//  a = d;
+//  d = temp;
+
+// console.log(a,b,c,d);
+
+// let tableau = [1, 2, 3, 4];
+// let temp;
+
+// temp = tableau[0];
+// tableau[0] = tableau[3];
+// tableau[3] = temp;
+// temp = tableau[1];
+// tableau[1] = tableau[2];
+// tableau[2] = temp;
+
+// console.log(tableau);
+
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+// let e = 5;
+// let f = 6;
+// let temp;
+
+// temp = b;
+// b = e;
+// e = f;
+// f = c;
+// c = a;
+// a = d;
+// d = temp;
+// console.log(e,b,a,f,d,c);
